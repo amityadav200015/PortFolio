f27539669294752c7b639308ec30f3d5b07b3bbc
Spencer <74230062+amityadav200015@users.noreply.github.com>
2021-05-18 21:00:57 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$(document).ready( function(){
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots'
+    });
+});
+
+$(document).ready( function(){
+    $('.card-wrapper2').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots2',
+        dotsClass:'dots'
+    });
+});
+
